a26c3efa37d176038cc557d99bd70b7f8d51581b
vinitha-vaitheesh <57670579+vinitha-vaitheesh@users.noreply.github.com>
2019-11-13 18:31:31 +0530

    EurekaServer

---
+++ EurekaServerProject/src/test/java/com/example/eureka/main/EurekaServerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureka.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
